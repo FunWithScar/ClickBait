a7a94dde8f67242219624b43718bcf28c34ac49e
FunWithScar <alexanderdepadro@gmail.com>
2026-07-09 15:54:52 -0400

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,1 +1,77 @@
-j
+// =====================================
+// Storage System
+// =====================================
+
+const Storage = {
+
+    KEY: "gamified-nonsense-save",
+
+    save() {
+
+        const saveData = {
+
+            stats: Game.stats,
+
+            unlocked: [...Game.unlocked]
+
+        };
+
+        localStorage.setItem(
+            this.KEY,
+            JSON.stringify(saveData)
+        );
+
+    },
+
+    load() {
+
+        const save =
+            localStorage.getItem(this.KEY);
+
+        if (!save)
+            return;
+
+        try {
+
+            const data = JSON.parse(save);
+
+            if (data.stats) {
+
+                Game.stats = data.stats;
+
+            }
+
+            if (data.unlocked) {
+
+                Game.unlocked =
+                    new Set(data.unlocked);
+
+            }
+
+        }
+
+        catch (e) {
+
+            console.error("Save file corrupted.", e);
+
+        }
+
+    },
+
+    clear() {
+
+        localStorage.removeItem(this.KEY);
+
+        location.reload();
+
+    }
+
+};
+
+// Auto Save Every 10 Seconds
+
+setInterval(() => {
+
+    Storage.save();
+
+},10000);
